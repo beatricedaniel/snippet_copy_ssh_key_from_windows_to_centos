b76437b78cc75e820721cd7462f2ebba6d2b5990
beatricedaniel <92784998+beatricedaniel@users.noreply.github.com>
2022-01-24 10:01:50 +0100

    Update copy_ssh_key_from_windows_to_centos.ps1

--- copy_ssh_key_from_windows_to_centos.ps1
+++ copy_ssh_key_from_windows_to_centos.ps1
@@ -1,1 +1,1 @@
-cat ~/.ssh/id_rsa_ferdiannd.pub | ssh devdata@20.199.125.243 "mkdir -p ~/.ssh && touch ~/.ssh/authorized_keys && chmod -R go= ~/.ssh && cat >> ~/.ssh/authorized_keys"
+cat ~/.ssh/id_rsa.pub | ssh devdata@20.199.125.243 "mkdir -p ~/.ssh && touch ~/.ssh/authorized_keys && chmod -R go= ~/.ssh && cat >> ~/.ssh/authorized_keys"
